4a55b3797d2e0c1c1be9e26d425dbe817c08dc42
Muhd-Naufal <198naufal@gmail.com>
2019-07-11 02:19:33 +0800

    FUCK YEA I DID IT!

--- Headline 2.R
+++ Headline 2.R
@@ -18,11 +18,13 @@ leaflet(mrtsg) %>%
   addTiles() %>% 
   addCircleMarkers(radius=4, stroke=2, fillColor = "Red", color="Red")
 
+#Create a new dataframe with lat and long being column names and as numeric
 example_points <- data.frame(lat=numeric(), long= numeric())
-example_points[1,] <- c(1.385361693,103.74436,7)
+example_points[1,] <- c(1.385361693,103.744367)
 
 
 #add coordinates to make it spatial data
+#reason why lat and long is because the dataframe that was created before this was named lat and long
 coordinates(example_points) <- ~long + lat
 
 pointsBuffer <- gBuffer(example_points, width=.05, byid = TRUE)
@@ -34,6 +36,7 @@ leaflet(mrtsg) %>%
   addPolygons(data=pointsBuffer)
 
 over(pointsBuffer, mrtsg, fn=length)
+over(pointsBuffer, mrtsg, returnList = TRUE)
 
 #############################################################################################
 
@@ -50,6 +53,11 @@ primaryschoolname <- primaryschool$Name
 coordinates(mrt) <- ~Longitude + Latitude
 coordinates(primaryschool) <- ~Longitude + Latitude
 
+#Combined test 
+combined <- read.csv("./Datasets/CombinedTest.csv")
+combined <- as.data.frame(combined)
+coordinates(combined) <- ~Longitude + Latitude
+
 leaflet() %>%
   addTiles() %>%
   addCircleMarkers(data=mrt,
@@ -61,4 +69,34 @@ leaflet() %>%
   addMarkers(data=primaryschool,
              popup = primaryschoolname)
 
-getcoords <- readline(prompt="Enter lat long:")
+example_points2 <- data.frame(lat=numeric(), long= numeric())
+example_points2[1,] <- c(1.385361693,103.744367)
+
+coordinates(example_points2) <- ~long + lat
+
+pointsBuffer2 <- gBuffer(example_points2, width=.01, byid = TRUE)
+
+leaflet() %>%
+  addTiles() %>%
+  addCircleMarkers(data=mrt,
+                   radius=4, 
+                   stroke=2, 
+                   fillColor = "Red", 
+                   color="Red",
+                   popup = stationname) %>%
+  addMarkers(data=primaryschool,
+             popup = primaryschoolname) %>%
+  addMarkers(data=example_points2) %>%
+  addPolygons(data=pointsBuffer2)
+  
+over(pointsBuffer2, mrt, fn=length)
+over(pointsBuffer2, mrt, returnList = TRUE)
+
+leaflet(options = leafletOptions(preferCanvas = TRUE)) %>%
+  addTiles() %>%
+  addMarkers(data=combined) %>%
+  addMarkers(data=example_points2) %>%
+  addPolygons(data=pointsBuffer2)
+
+over(pointsBuffer2, combined, fn=length)
+over(pointsBuffer2, combined, returnList = TRUE)
