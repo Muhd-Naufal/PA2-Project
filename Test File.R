98933824b29b4cbae883ad229cedd8d5ac74b686
Muhd-Naufal <1998naufal@gmail.com>
2019-07-10 15:57:49 +0800

    added buffer and marker point for analysis

--- Test File.R
+++ Test File.R
@@ -181,3 +181,35 @@ m <- m %>% addPolygons(
     direction = "auto"))
 m
 
+#install.packages("rgeos")
+library(rgeos)
+
+mrt<-read.csv("mrtsg.csv")
+mrt<- as.data.frame(mrt)
+stationname<-mrt$STN_NAME
+stationname
+#CCK lat long example point
+#1.385361693	103.744367
+
+
+mrtsg <- mrt
+coordinates(mrtsg) <- ~Longitude + Latitude
+
+leaflet(mrtsg) %>% 
+  addTiles() %>% 
+  addCircleMarkers(radius=4, stroke=2, fillColor = "Red", color="Red")
+
+example_points <- data.frame(lat=numeric(), long= numeric()) 
+example_points[1,] <- c(1.385361693,103.744367)
+
+#add coordinates to make it spatial data
+coordinates(example_points) <- ~long + lat
+
+pointsBuffer <- gBuffer(example_points, width=.05, byid = TRUE)
+
+leaflet(mrtsg) %>% addTiles() %>% addCircleMarkers(radius=4, stroke=2, fillColor = "Red", color="Red") %>%
+  addMarkers(data=example_points) %>%
+  addPolygons(data=pointsBuffer)
+
+over(pointsBuffer, mrtsg, fn=length)
+
