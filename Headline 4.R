932cd1f0197de48bf7ba9e4f07d8f00ada47e661
mvhdashraf <ashrafbb@gmail.com>
2019-07-13 20:30:05 +0800

    a

--- Headline 4.R
+++ Headline 4.R
@@ -6,3 +6,24 @@ library(leaflet)
 library(geojsonio)
 library(RColorBrewer)
 library(rgeos)
+
+mature<-read.csv("./Datasets/maturetowns.csv")
+mature
+nonmature<-read.csv("./Datasets/nonmaturetowns.csv")
+nonmature
+
+max(mature$resale_price)
+min(mature$resale_price)
+
+max(nonmature$resale_price)
+min(nonmature$resale_price)
+
+library(ggplot2)
+ggplot(mature, aes(x=mature$month, y=mature$resale_price))+
+  geom_point(stat="identity",color ="red") +
+  ggtitle("Resale Price of Mature Towns by Year-Month") +
+  xlab("year-month") + ylab("Resale Price")
+
+
+
+
