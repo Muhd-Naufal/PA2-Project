ca16587606899a80050b8c62dcb41b7ef4898d74
mvhdashraf <ashrafbb@gmail.com>
2019-07-13 21:39:42 +0800

    a

--- Headline 4.R
+++ Headline 4.R
@@ -7,23 +7,39 @@ library(geojsonio)
 library(RColorBrewer)
 library(rgeos)
 
+
 mature<-read.csv("./Datasets/maturetowns.csv")
 mature
 nonmature<-read.csv("./Datasets/nonmaturetowns.csv")
 nonmature
 
-max(mature$resale_price)
-min(mature$resale_price)
+maxmature<-max(mature$resale_price)
+minmature<-min(mature$resale_price)
 
-max(nonmature$resale_price)
-min(nonmature$resale_price)
+maxnonmature<-max(nonmature$resale_price)
+minnonmature<-min(nonmature$resale_price)
+
+mature1<-c(maxmature,minmature)
+nonmature1<-c(maxnonmature,minnonmature)
+
+data<-data.frame(mature1,nonmature1)
+data
+
+rownames(data)<-c("max","min")
+data
 
 library(ggplot2)
-ggplot(mature, aes(x=mature$month, y=mature$resale_price))+
-  geom_point(stat="identity",color ="red") +
-  ggtitle("Resale Price of Mature Towns by Year-Month") +
-  xlab("year-month") + ylab("Resale Price")
+library(reshape2)
+
+melt(data,id.vars="",measure.vars = c("max","min"))
 
 
-
-
+ggplot(data,aes(x=max,y=max(mature$resale_price),fill=variable))+
+  geom_bar(stat="identity",position='dodge')+
+ggplot(nonmature,aes(x=town,y=max(nonmature$resale_price),fill=variable))+
+  geom_bar(stat="identity",position='dodge')+
+  ggtitle("Mature vs Non-Mature")+xlab("town")+ylab("resale_price")+
+  theme(
+    plot.title=element_text(color="red",size=12,face="bold.italic",hjust=0.5),
+    axis.title.x = element_text(color="blue",size=12,face="bold"),
+    axis.title.y = element_text(color="green",size=12,face="bold"))
