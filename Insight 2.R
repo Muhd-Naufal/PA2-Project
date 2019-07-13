85c4348135f9b377e55b9e44965c983b1ca52720
mvhdashraf <ashrafbb@gmail.com>
2019-07-13 18:24:05 +0800

    a

--- Insight 2.R
+++ Insight 2.R
@@ -1,1 +1,13 @@
 #High Resale Prices
+
+hdb<-read.csv("insight 2.csv")
+hdb
+
+library(ggplot2)
+ggplot(hdb,aes(x=hdb$flat_model,y=hdb$resale_price)) +
+  geom_point(fill="pink",color ="red")
+
+ggplot(hdb,aes(x=hdb$flat_model,y=hdb$resale_price)) +
+  geom_smooth( fill = "pink", colour = "red",se=FALSE)
+  geom_smooth(method="lm",se=FALSE)
+
