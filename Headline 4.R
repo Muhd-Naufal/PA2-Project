40b6cb37c048559d746fcb477946b92e9f7f9f01
mvhdashraf <ashrafbb@gmail.com>
2019-07-13 21:54:07 +0800

    a

--- Headline 4.R
+++ Headline 4.R
@@ -13,17 +13,26 @@ mature
 nonmature<-read.csv("./Datasets/nonmaturetowns.csv")
 nonmature
 
+
 maxmature<-max(mature$resale_price)
 minmature<-min(mature$resale_price)
 
 maxnonmature<-max(nonmature$resale_price)
 minnonmature<-min(nonmature$resale_price)
 
+a <- max(mature$resale_price)
+b <- min(mature$resale_price)
+
+c <- max(nonmature$resale_price)
+d <- min(nonmature$resale_price)
+
+
 mature1<-c(maxmature,minmature)
 nonmature1<-c(maxnonmature,minnonmature)
 
 data<-data.frame(mature1,nonmature1)
 data
+
 
 rownames(data)<-c("max","min")
 data
@@ -32,6 +41,30 @@ library(ggplot2)
 library(reshape2)
 
 melt(data,id.vars="",measure.vars = c("max","min"))
+
+ employee <- c('John Doe','Peter Gynn','Jolie Hope')
+ salary <- c(21000, 23400, 26800)
+ startdate <- as.Date(c('2010-11-1','2008-3-25','2007-3-14'))
+
+ employ.data <- data.frame(employee, salary, startdate)
+ employ.data
+
+ maturetown <- c(a,b)
+ nonmaturetown <- c(c,d)
+ 
+ town <- data.frame(maturetown,nonmaturetown)
+ town
+ 
+id <- c(1,2,3,4)
+town2 <-cbind(id,town)
+town2
+ 
+town3 <- melt(town,id='id')
+town3
+
+ggplot(data = town3, aes(x=id, y=value, fill=variable)) +
+  geom_bar(stat = 'identity',position = 'dodge')
+
 
 
 ggplot(data,aes(x=max,y=max(mature$resale_price),fill=variable))+
