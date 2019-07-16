17244541aca9cf7b3f3db459c082cb77f7bb135b
Muhd-Naufal <198naufal@gmail.com>
2019-07-16 21:28:35 +0800

    d

--- a test.R
+++ a test.R
@@ -1,36 +1,26 @@
-#Animation test
+#Anime test
 library(ggplot2)
 library(gganimate)
-install.packages(g)
 library(dplyr)
 
 housing_data <- read.csv("./Datasets/HDB Resale Prices.csv")
+housing_data
 
-#Convert Factor to Date (Year-Month)
-#It'll assume first day of each month
+str(housing_data)
 
-housing_data$month <- as.Date(paste0(housing_data$month, "-01"), "%Y-%m-%d")
+housing_data2 <- housing_data[,c(1,2,10)]
+housing_data2
+str(housing_data2)
 
-#Choose only those 2012 and above
-library(dplyr)
-housing_data2012 <- filter(housing_data, month >= "2012-01-01")
+i <- sapply(housing_data2, is.factor)
+housing_data2[i] <- lapply(housing_data2[i], as.character)
+
+str(housing_data2)
+
+housing_data2012 <- filter(housing_data2, month >= "2012-01-01")
 housing_data2012
 
 
-i <- sapply(housing_data, is.factor)
-housing_data[i] <- lapply(housing_data[i], as.character)
-
-
-
 library(ggplot2)
-ggplot(housing_data2012, aes(x=town, y=resale_price, label = town, color = town, group= town)) +
-  geom_point(stat= 'identity', size = 15) +
-  geom_segment(aes(
-    y = 100,
-    x = town,
-    yend = resale_price,
-    xend = town)
-  ) +
-  geom_text(color="black", size=3) +
-  coord_flip() +
-  theme(legend.position ="none")
+ggplot(housing_data2012, aes(x=town, y=resale_price), group_by(town)) +
+  geom_point(stat='identity')
