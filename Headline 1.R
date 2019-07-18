d5166cb57456c3c79412a141ec8eae3e3a4919cc
Muhd-Naufal <1998naufal@gmail.com>
2019-07-18 19:58:21 +0800

    fwef

--- Headline 1.R
+++ Headline 1.R
@@ -10,9 +10,15 @@ library(dplyr)
 Punggol$month <- as.Date(paste0(Punggol$month, "-01"), "%Y-%m-%d")
 Punggol
 
+Punggol$year <- format(as.Date(Punggol$month, format="%Y-%m-%d"),"%Y")
+Punggol
+
 #Choose only those 2007 and above
 
 library(dplyr)
 Punggol2 <- filter(Punggol, month >= "2007-01-01",town == "PUNGGOL")
 Punggol2
+
+levels(Punggol2$flat_type)
+
 
