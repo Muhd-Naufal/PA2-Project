19827d5d6dad21c7a440e905298f80b85168be9c
mvhdashraf <ashrafbb@gmail.com>
2019-07-18 19:54:35 +0800

    a

--- Headline 1.R
+++ Headline 1.R
@@ -16,3 +16,18 @@ library(dplyr)
 Punggol2 <- filter(Punggol, month >= "2007-01-01",town == "PUNGGOL")
 Punggol2
 
+type2<-filter(Punggol2, Punggol2$flat_type=="2 ROOM")
+a<-mean(type2$resale_price)
+a
+
+type3<-filter(Punggol2, Punggol2$flat_type=="3 ROOM")
+b<-mean(type3$resale_price)
+b
+
+type4<-filter(Punggol2, Punggol2$flat_type=="4 ROOM")
+c<-mean(type4$resale_price)
+c
+
+type5<-filter(Punggol2, Punggol2$flat_type=="5 ROOM")
+d<-mean(type5$resale_price)
+d
