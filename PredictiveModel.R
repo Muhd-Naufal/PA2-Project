10bee7dbf702a0ff15470c8fd701c06de50e32c2
Javier <Javierloh1190@gmail.com>
2019-07-19 20:10:31 +0800

    h

--- PredictiveModel.R
+++ PredictiveModel.R
@@ -29,8 +29,8 @@ fit2 = auto.arima(log10(monthly_ts), approximation = F,trace= F)
 summary(fit2)
 
 prediction = predict(fit2, n.ahead=10)
-plot(monthly_ts, type ="l", xlab="", ylab = "Average Price per Square Meter", xlim = c(2012,2019),ylim = c(4500,6500), main="ARIMA prediction")
-lines(10^(prediction$pred),col="blue")
-lines(10^(prediction$pred+2*prediction$se),col="orange")
+plot(monthly_ts, type ="l", xlab="", ylab = "Average Price per Square Meter", xlim = c(2012,2019),ylim = c(4500,6500), main="ARIMA prediction") +
+lines(10^(prediction$pred),col="blue") +
+lines(10^(prediction$pred+2*prediction$se),col="orange") +
 lines(10^(prediction$pred-2*prediction$se),col="orange")
 
